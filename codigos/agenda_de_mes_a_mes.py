343615826c35a2bb49773ffc41ee1255f3ea2b60
iastoan <lucenacortezgustavo@gmail.com>
2025-11-25 22:03:58 -0300

    atualização da iplementação em portgues e explixação do codigo

--- codigos/agenda_de_mes_a_mes.py
+++ codigos/agenda_de_mes_a_mes.py
@@ -1,14 +1,21 @@
 import calendar
-
+#a função do import traz o calendario no codigo sem ter quer colocar dia por dia.
+import locale
+#a funçaõ locale e usada nesse cenario para colocar o calendario em portguês
+locale.setlocale(locale.LC_ALL, "pt_BR.UTF-8")
+#colocando a definição da lingua que eu quero nesse caso português 
 ano=2025
 mes=1
+#variaveis dizendo o mes e ano
 resultado_mes =calendar.month(ano,mes)
+#uma formula que mostrar todo o mes especifico dito e o ano que e.
 print(resultado_mes)
 
 ano=2025
 mes=2
 resultado_mes =calendar.month(ano,mes)
 print(resultado_mes)
+#com o resto sendo copia com mês e ano diferentes.
 
 ano=2025
 mes=3
@@ -118,4 +125,5 @@ print(resultado_mes)
 ano=2026
 mes=12
 resultado_mes =calendar.month(ano,mes)
+
 print(resultado_mes)
